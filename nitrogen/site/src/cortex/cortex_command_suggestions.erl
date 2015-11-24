1ed3ec6ad68535cbbd48f4674a5172ab0ca47711
Jesse Stimpson <jstimpson@bandwidth.com>
2015-11-24 10:28:52 -0500

    suggest nodes

--- nitrogen/site/src/cortex/cortex_command_suggestions.erl
+++ nitrogen/site/src/cortex/cortex_command_suggestions.erl
@@ -1,8 +1,20 @@
 -module(cortex_command_suggestions).
 
--export([get/3]).
+-export([get/3, get_nodes/0]).
 
 get(iswitch_node_offline, 'Node', _Src) ->
-    [test];
+    get_nodes();
 get(_, _, _) ->
     [].
+
+get_nodes() ->
+    {RevReach, RevUnreach} =
+    ets:foldl(fun({{_,Node}, Data}, {Reach, Unreach}) ->
+                    case proplists:get_value(reachable, Data) of
+                        node_reachable ->
+                            {[Node|Reach], Unreach};
+                        node_unreachable ->
+                            {Reach, [Node|Unreach]}
+                    end
+            end, {[], []}, app_reachability),
+    lists:usort(RevUnreach) ++ lists:usort(RevReach).
